e2a2715022e671905b0e70b92b4f03f250253e81
Tomasz Zajac <tomezpl@gmail.com>
2020-02-26 10:43:58 +0000

    workshop3

--- Tutorial 3/Tutorial 3.cpp
+++ Tutorial 3/Tutorial 3.cpp
@@ -95,7 +95,7 @@ int main(int argc, char **argv) {
 		queue.enqueueFillBuffer(buffer_B, 0, 0, output_size);//zero B buffer on device memory
 
 		//4.2 Setup and execute all kernels (i.e. device code)
-		cl::Kernel kernel_1 = cl::Kernel(program, "reduce_add_1");
+		cl::Kernel kernel_1 = cl::Kernel(program, "reduce_add_2");
 		kernel_1.setArg(0, buffer_A);
 		kernel_1.setArg(1, buffer_B);
 //		kernel_1.setArg(2, cl::Local(local_size*sizeof(mytype)));//local memory size
